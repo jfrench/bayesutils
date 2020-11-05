3fd6c1372dd3eb6764bfb0509f3a0a59e36399e2
jfrench <joshua.french@gmail.com>
2020-11-05 09:17:51 -0700

    Add bank_salary data.

---
+++ R/bank_salary.R
@@ -0,0 +1,24 @@
+#' @name bank_salary
+#' @title Bank Salary Data
+#' @description This data set contains 93 observations related to bank employee
+#'   salary for entry-level clerical employees hired by the Harris Trust and
+#'   Savings Bank between 1969 and 1977.
+#'
+#' @docType data
+#' @usage data(bank_salary)
+#'
+#' @format A data frame with 93 rows and 5 columns:
+#' \describe{
+#'  \item{bsalary}{Beginning employee salary in thousands of dollars.}
+#'  \item{male}{An indicator variable for whether the employee is male.}
+#'  \item{education}{Years of education.}
+#'  \item{experience}{Months of experience.}
+#'  \item{time}{Time at hiring as measured in months after January 1, 1969.}
+#' }
+#' @source Ramsey,  F.L.  and  Schafer,  D.W.  (2013).The  Statistical  Sleuth:
+#'   A  Course  in  Methods  of  DataAnalysis (3rd ed), Cengage Learning
+#' @references Roberts, H.V. (1979).  Harris Trust and Savings Bank:  An
+#'   Analysis of Employee Compensation,Report 7946, Center for Mathematical
+#'   Studies in Business and Economics, University of ChicagoGraduate School of
+#'   Business.
+NULL
