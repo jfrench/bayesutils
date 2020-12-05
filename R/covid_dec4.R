de54a003b29b35fa0005f648f46e7b3c01c9728d
jfrench <joshua.french@gmail.com>
2020-12-05 14:50:25 -0700

    Add covid_dec4 data set.

---
+++ R/covid_dec4.R
@@ -0,0 +1,21 @@
+#' @name covid_dec4
+#' @title COVID-19 deaths
+#' @description This data set contains the number of confirmed COVID-19 deaths
+#' in U.S. states through December 4th, 2020 along with some very basic
+#' demographic information.
+#'
+#' @docType data
+#' @usage data(covid_dec4)
+#'
+#' @format A data frame with 50 rows and 7 variables
+#' \describe{
+#'   \item{state_name}{state name}
+#'   \item{state_abb}{state name abbreviation}
+#'   \item{deaths}{confirmed number of COVID-19 deaths}
+#'   \item{population}{population e5-year stimate from the 2017 American Community Survey}
+#'   \item{income}{median income (U.S. dollars) 5-year estimate from the 2017 American Community Survey}
+#'   \item{hs}{estimated percentage of population > 25 years old with a high school diploma based on the 2010 Census}
+#'   \item{bs}{estimated percentage of population > 25 years old with a Bacherlor's degree based on the 2010 Census}
+#' }
+#' @source <https://covidtracking.com/data/download> (Accessed 2020-12-04) and <https://www.census.gov>
+NULL
