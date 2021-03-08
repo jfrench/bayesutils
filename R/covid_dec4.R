f44f3591efce3a8a6de1b89206ca19c87e6a1007
jfrench <joshua.french@gmail.com>
2021-03-08 15:32:36 -0700

    Add covid_20210307

--- R/covid_dec4.R
+++ R/covid_dec4.R
@@ -1,9 +1,9 @@
 #' @name covid_dec4
 #' @title COVID-19 deaths
-#' @description This data set contains the number of COVID-19 deaths
-#' in U.S. states through December 4th, 2020 along with some very basic
-#' demographic information. While the data are taken from official sources,
-#' they are only intended for demonstration purposes.
+#' @description This data set contains the number of confirmed and probable
+#'   COVID-19 deaths in U.S. states through December 4th, 2020 along with some
+#'   very basic demographic information. While the data are taken from official
+#'   sources, they are only intended for demonstration purposes.
 #'
 #' @docType data
 #' @usage data(covid_dec4)
@@ -12,11 +12,16 @@
 #' \describe{
 #'   \item{state_name}{state name}
 #'   \item{state_abb}{state name abbreviation}
-#'   \item{deaths}{confirmed number of COVID-19 deaths}
-#'   \item{population}{population e5-year stimate from the 2017 American Community Survey}
-#'   \item{income}{median income (U.S. dollars) 5-year estimate from the 2017 American Community Survey}
-#'   \item{hs}{estimated percentage of population > 25 years old with a high school diploma based on the 2010 Census}
-#'   \item{bs}{estimated percentage of population > 25 years old with a Bacherlor's degree based on the 2010 Census}
+#'   \item{deaths}{confirmed and probable number of COVID-19 deaths}
+#'   \item{population}{population 5-year estimate from the 2017 American
+#'   Community Survey}
+#'   \item{income}{median income (U.S. dollars) 5-year
+#'   estimate from the 2017 American Community Survey}
+#'   \item{hs}{estimated
+#'   percentage of population > 25 years old with a high school diploma based on
+#'   the 2010 Census}
+#'   \item{bs}{estimated percentage of population > 25 years
+#'   old with a Bachelor's degree based on the 2010 Census}
 #' }
 #' @source <https://covidtracking.com/data/download> (Accessed 2020-12-04) and <https://www.census.gov>
 NULL
